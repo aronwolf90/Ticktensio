26b7e9849798a9fe52943b8929089d18f1a218fd
Aron Wolf <aronwolf90@gmail.com>
2021-06-20 18:10:16 +0200

    fix: do not infect with 404 css the other pages
    Solution: scope the css.

--- frontend/src/pages/404.vue
+++ frontend/src/pages/404.vue
@@ -1,11 +1,12 @@
 <template lang='pug'>
-.page-wrap.d-flex.flex-row.align-items-center
-  .container
-     .row.justify-content-center
-       .col-md-12.text-center
-         span.display-1.d-block 404
-         .mb-4.lead The page you are looking for was not found.
-         a.btn.btn-link(href="/") Back to Home
+.not-found
+  .page-wrap.d-flex.flex-row.align-items-center
+    .container
+       .row.justify-content-center
+         .col-md-12.text-center
+           span.display-1.d-block 404
+           .mb-4.lead The page you are looking for was not found.
+           a.btn.btn-link(href="/") Back to Home
 </template>
 
 <script>
@@ -13,8 +14,8 @@ export default {
 }
 </script>
 
-<style lang='sass'>
-body
+<style lang='sass' scoped>
+.not-found
   background: #dedede
 
 .page-wrap
